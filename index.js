ec3d7ad248a11151d1a4fb281225e2a85deaa9f1
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2018-09-20 12:15:23 -0700

    Made some edits to excessive codes in spots, mostly final edit

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 //Note: Sometimes(Not often) countries are slow to load from API, give it a sec to populate search form
 'use strict'
 
-const Url="https://api.data.gov/ed/collegescorecard/v1/schools.json?";
-let globalCountry = undefined;
-
 //EVENT LISTENER FOR COUNTRY SELECTION
 function watchCountrySubmit(){
   $('.country-question-form').submit(function(event){
     event.preventDefault();
-    $('.chosen-result').html('').addClass('hidden'); //Clear Previous Selection
+    clearDiv();
     $('.js-results-holder').html('').addClass('hidden'); // Clear Previous Selection
-    $('.js-youtube-holder').html('').addClass('hidden'); // Clear Previous Selection
-    $('.js-pop-info').html('').addClass('hidden'); // Clear Previous Selection
-    $('.js-poprace-info').html('').addClass('hidden'); // Clear Previous Selection
-    // watchSubmit();
-
+    
     //Handles Orange Check Mark for Search Forms
     $('.orange-check-school').html('');
     $('.orange-check-country').html('').append("<img src=orange_mark.png alt='orange-check-country'>");
@@ -26,35 +19,33 @@ function watchCountrySubmit(){
 function watchSubmit(){
   $('#js-search-form').submit(function(event) {
     event.preventDefault();
-    $('.js-results-holder').removeClass('hidden');
-    $('.chosen-result').html('').addClass('hidden'); //Clear Previous Selection
-    $('.js-pop-info').html('').addClass('hidden'); // Clear Previous Selection
-    $('.js-poprace-info').html('').addClass('hidden'); // Clear Previous Selection
-    $('.js-results-holder').html('');//Clear Previous Selection
-    $('.js-youtube-holder').html('').addClass('hidden');
+    clearDiv();
+    $('.js-results-holder').removeClass('hidden').html('');
     $('.results-header').removeClass('hidden'); //Reveal Header for Results
     const userSubmission = $('#js-search-box').val(); //Save user search term to userSubmission
     $('#js-search-box').val(''); //Clear Value after saving search term
     getCollegesApi(userSubmission);
+    
     //Sets up Results Holder
     $('.js-results-holder').addClass('fade-in').css("background-color", "peachpuff").html('').append(
           `<h2>Select a college to view tuition in currency of ${globalCountry}</h2>`)  
+    
     //Coordinates Orange Check Mark for Search Forms
     $('.orange-check-country').html('');
     $('.orange-check-school').html('').append("<img src=orange_mark.png alt='orange-check-school'>");
-    // clear out the input
  });
 }
 
 //GET COLLEGES API() makes JSON call for data
 function getCollegesApi(searchTerm){
+  const Url="https://api.data.gov/ed/collegescorecard/v1/schools.json?";
   const query = {
         "school.name": `${searchTerm}`,
         _fields: "school.name,id,latest.cost.tuition.out_of_state,latest.cost.tuition.in_state,school.school_url,latest.student.size,latest.student.demographics.non_resident_aliens_2000,latest.student.demographics.race_ethnicity.white,latest.student.demographics.race_ethnicity.black,latest.student.demographics.race_ethnicity.hispanic,latest.student.demographics.race_ethnicity.asian",
         api_key: "8P336smWdRHaUwK6gjNbmGzeaoRDEqyIt16jEInQ",
         _per_page: "100"
   }
-  
+
   $.getJSON(Url, query, loadColleges);
 
 //Loads College Data
@@ -72,7 +63,6 @@ function loadColleges(data){
       let hispanic = data.results[`${i}`][`latest.student.demographics.race_ethnicity.hispanic`]
       let asian = data.results[`${i}`][`latest.student.demographics.race_ethnicity.asian`]
       
-
     //Compare in state tuition and out of state tuition to see if returns null --> want to omit results that have no data
       if(college_tuition_out_state === null && college_tuition_in_state === null){
         //Exclude Results
@@ -80,8 +70,6 @@ function loadColleges(data){
       else if(college_tuition_out_state === null){
         $(`<a href="#top"><h3 class="result_school_name result_school_name${i}"> ${college_name}</h3></a>`).appendTo('.js-results-holder');
         college_tuition_out_state = college_tuition_in_state;
-        //IF NVDA is on, you can tab through results and open them with enter...if it is off, it only tabs through results and takes you to top
-
       }
       else{
         $(`<a href="#top"><h3 class="result_school_name result_school_name${i}"> ${college_name}</h3></a>`).appendTo('.js-results-holder'); 
@@ -89,48 +77,40 @@ function loadColleges(data){
 
       //Event Listener for apending CHOSEN school information
       $(`.result_school_name${i}`).on('click', function(){
-        $('.chosen-result').removeClass('hidden');
-        $('.js-youtube-holder').removeClass('hidden');
-        $('.js-pop-info').removeClass('hidden');
-        $('.js-poprace-info').removeClass('hidden');
-        $(`.chosen-result`).addClass('fade-in').html('').append(
+        removeHidden();
+        $('.chosen-result').addClass('fade-in').html('').append(
           `<h2>${college_name}<p>See more at - <a href="http://${college_url}" target="_blank">${college_url}</a></p></h2>
              
            <div>
               <p>Tuition in ${globalCurrencyId}</p>
               <p>${globalSymbol} ${Math.trunc(globalRate * college_tuition_out_state).toLocaleString()}</p><br>
               <p>About $${Math.trunc(college_tuition_out_state).toLocaleString()} USD</p>
-
            </div></br>`)
            
-           $(`.js-pop-info`).addClass('fade-in').html('').append(
-            `<h2>Degree-Seeking Student Population<h2>
-                <h3>${(population).toLocaleString()}</h3>
-             <h2>Percentage of Foreign Students</h2>
-                <h3>${(aliens * 100).toFixed(2)}%</h3>
-             </div></br>`)
+        $('.js-pop-info').addClass('fade-in').html('').append(
+           `  <h2>Degree-Seeking Student Population<h2>
+              <h3>${(population).toLocaleString()}</h3>
+              <h2>Percentage of Foreign Students</h2>
+              <h3>${(aliens * 100).toFixed(2)}%</h3>
+              </br>`)
 
-          $(`.js-poprace-info`).addClass('fade-in').html('').append(
-               `<h2> Racial Demographics (Population) <h2>
-                  <h3>White Students<br> ${(white * 100).toFixed(2)}%</h2>
-                  <h3>Black Students<br> ${(black * 100).toFixed(2)}%</h2>
-                  <h3>Hispanic Students<br> ${(hispanic * 100).toFixed(2)}%</h2>
-                  <h3>Asian Students<br> ${(asian * 100).toFixed(2)}%</h2>
-               </div></br>`)
-        
+        $('.js-poprace-info').addClass('fade-in').html('').append(
+           `<h2> Racial Demographics (Population) <h2>
+               <h3>White Students<br> ${(white * 100).toFixed(2)}%</h2>
+               <h3>Black Students<br> ${(black * 100).toFixed(2)}%</h2>
+               <h3>Hispanic Students<br> ${(hispanic * 100).toFixed(2)}%</h2>
+               <h3>Asian Students<br> ${(asian * 100).toFixed(2)}%</h2>
+               </br>`)
+
            window.scrollTo(0,0);
            $('.js-youtube-holder').addClass('fade-in').html('');
            getDataFromYoutubeApi(`${college_name}`, displayYoutubeSearchData); 
-
       })
     }
-
-    // $('.submit-school-button').on('click', function(event){
+      //Handles No Results
       if(data.results.length < 1){
         $('.js-results-holder').append("<p class='no-results'>No results - Try again</p>");
-      }
-    // })
- 
+    } 
   } 
 }
 
@@ -143,6 +123,7 @@ const symbolArray = [];
 let globalRate = undefined;
 let globalCurrencyId = undefined;
 let globalSymbol = undefined;
+let globalCountry = undefined;
 
 //Calls for data from country api and loads them for appending
 function getCountryApi(){
@@ -153,20 +134,12 @@ function getCountryApi(){
 
 function loadCountries(data){
   const results = data.results;
-  console.log(results);
   for(let countryCode in results){
     $('#country-choices').append(`<option value="${results[countryCode].name}"> ${results[countryCode].name} </option>`);
     countryIdArray.push(results[countryCode].name);
     currencyIdArray.push(results[countryCode].currencyId);
     symbolArray.push(results[countryCode].currencySymbol);
   } 
-    // let listCountryIdArray = countryIdArray.sort();
-    // console.log(countryIdArray);
-  
-  // for(let i=0; i<countryIdArray.length; i++){
-  //   $('#country-choices').append(`<option value="${listCountryIdArray[i]}"> ${listCountryIdArray[i]} </option>`);
-  // } //Also changed const to let for countryIdArray
-
     watchSubmitCountry(data);
 }
 
@@ -175,10 +148,8 @@ function watchSubmitCountry(){
   $('.country-question-form').submit(event => {
     event.preventDefault();
     const userSubmission =  $(`#country-choices`).val();
-    const countryChoice = userSubmission;
     globalCountry = userSubmission;
     $('#country-choices').val('');
-    console.log(`Country: ${countryChoice}`);
     
 //Matches country choice to currency    
 function matchCurrencyId(){
@@ -186,11 +157,10 @@ function matchCurrencyId(){
       if(userSubmission === countryIdArray[i]){
         globalCurrencyId = currencyIdArray[i];
         globalSymbol = symbolArray[i]; 
-        console.log(`The currency Id is: ${globalCurrencyId}`)
         getConverterApi(globalCurrencyId);     
-     }
+        }
+      }
     }
-  }
     matchCurrencyId();
   })
 }
@@ -198,31 +168,28 @@ function matchCurrencyId(){
 //_________________________________________________________________________________________________________________________________________
 //Call conversion data from converter api
 function getConverterApi(currencyIdValue){
-  const Url="https://free.currencyconverterapi.com/api/v6/convert"
+    const Url="https://free.currencyconverterapi.com/api/v6/convert"
+    const query = {
+      q: `USD_${currencyIdValue}`,
+      compact:"ultra"
+    }
+     $.getJSON(Url, query, testConversion);
 
-  const query = {
-    q: `USD_${currencyIdValue}`,
-    compact:"ultra"
-  }
-  $.getJSON(Url, query, testConversion);
-
-function testConversion(data){
-    console.log(data);
-    const rate = data[query.q];
-    console.log(`The conversion rate is: ${rate}`);
-    globalRate = rate;
-    getCollegesApi();
-  }
+    function testConversion(data){
+      const rate = data[query.q];
+      globalRate = rate;
+      getCollegesApi();
+    }
 }
 
 //__________________________________________________________________________________________________________________________________________
-//Youtube Stuff
+//Handles Youtube
 
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 function getDataFromYoutubeApi(searchTerm, callback) {
   
-    const query = {
+  const query = {
     key: "AIzaSyDugJKdQUUz5eiD3aWn9_5pL8ybtaKDPno",
     part: "snippet",
     q: `${searchTerm} international students`,
@@ -248,18 +215,30 @@ function renderResult(item) {
 function displayYoutubeSearchData(data) {
   const items = data.items.map((item, index) => renderResult(item));
   if(data.items < 1){
-  $('.js-youtube-holder').append("<h2 class='no-results'>No results - Try again</h2>");
-  // $('.js-youtube-holder').append(data.pageInfo.totalResults);
+      $('.js-youtube-holder').append("<h2 class='no-results'>No results - Try again</h2>");
   }
-
   else{
-    $('.js-youtube-holder').append(items);
+      $('.js-youtube-holder').append(items);
   }
 }
 
+//_____________________________________________________________________________________________
+//Clear Previous Information or Selection
+function clearDiv(){
+  $('.js-youtube-holder').html('').addClass('hidden'); 
+  $('.js-pop-info').html('').addClass('hidden'); 
+  $('.js-poprace-info').html('').addClass('hidden'); 
+  $('.chosen-result').html('').addClass('hidden'); 
+}
 
+function removeHidden(){
+  $('.chosen-result').removeClass('hidden');
+  $('.js-youtube-holder').removeClass('hidden');
+  $('.js-pop-info').removeClass('hidden');
+  $('.js-poprace-info').removeClass('hidden');
+}
 
-//
+//_____________________________________________________________________________________________
 function handleFunctions(){
   getCountryApi();
   watchCountrySubmit();
@@ -267,8 +246,4 @@ function handleFunctions(){
 }
 
 $(handleFunctions);
-
-// $('.js-youtube-holder').ready(function() {
-//   $('#loading').hide();
-// });
 
